b8bc4e36d2e9d716cebf82e997d7a29827b93da0
Steven <reversescale@icloud.com>
2018-05-04 10:14:58 +0800

    🚚:Update the upload configuration file regularly

---
+++ NetworkArchitectureDemo/Pods/Target Support Files/Pods-NetworkArchitectureDemo/Pods-NetworkArchitectureDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkArchitectureDemo : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkArchitectureDemo
+@end
---
+++ NetworkArchitectureMVCDemo/Pods/Target Support Files/Pods-NetworkArchitectureMVCDemo/Pods-NetworkArchitectureMVCDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkArchitectureMVCDemo : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkArchitectureMVCDemo
+@end
