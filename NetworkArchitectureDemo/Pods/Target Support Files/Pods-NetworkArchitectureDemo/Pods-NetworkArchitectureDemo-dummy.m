e736019ba6bd3ea6b03d67f9378ca22b54142661
ReverseScale <ReverseScale@iCloud.com>
2019-03-05 17:16:52 +0800

    🗑:Removing compiled files or packages.

--- NetworkArchitectureDemo/Pods/Target Support Files/Pods-NetworkArchitectureDemo/Pods-NetworkArchitectureDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetworkArchitectureDemo : NSObject
-@end
-@implementation PodsDummy_Pods_NetworkArchitectureDemo
-@end
--- NetworkArchitectureDemo/Pods/Target Support Files/Pods-NetworkArchitectureDemoTests/Pods-NetworkArchitectureDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetworkArchitectureDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_NetworkArchitectureDemoTests
-@end
